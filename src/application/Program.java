846d879b1ed12d1a2061fc08f93564d0d4fade65
Nathanael <vieira_trabalho@hotmail.com>
2022-01-19 21:12:11 -0300

    02 - frequencia de lancamentos

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,18 +5,44 @@ import java.security.SecureRandom;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		// Lançamento de dados.
 
-		SecureRandom sr = new SecureRandom();
+		SecureRandom rn = new SecureRandom();
 
-		for (int i = 1; i <= 20; i++) {
-			int randomValue = 1 + sr.nextInt(6);
-			System.out.printf("%d ", randomValue);
-			if (i % 5 == 0) {
-				System.out.println();
+		int frenquency1 = 0;
+		int frenquency2 = 0;
+		int frenquency3 = 0;
+		int frenquency4 = 0;
+		int frenquency5 = 0;
+		int frenquency6 = 0;
+
+		for (int i = 1; i < 6000000; i++) {
+			int face = 1 + rn.nextInt(6);
+
+			switch (face) {
+			case 1:
+				++frenquency1;
+				break;
+			case 2:
+				++frenquency2;
+				break;
+			case 3:
+				++frenquency3;
+				break;
+			case 4:
+				++frenquency4;
+				break;
+			case 5:
+				++frenquency5;
+				break;
+			case 6:
+				++frenquency6;
+				break;
 			}
 		}
+		
+		System.out.println("Face\tFrequency");
+		System.out.printf("1\t%d%n2\t%d%n3\t%d%n4\t%d%n5\t%d%n6\t%d%n", frenquency1, 
+				frenquency2, frenquency3, frenquency4, frenquency5, frenquency6);
 
 	}
 
